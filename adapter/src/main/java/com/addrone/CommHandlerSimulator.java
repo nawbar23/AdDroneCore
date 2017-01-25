5869d5083926d508f879f4c0b29bd29fdf2f2689
Kacper Kowalcze <kacper.kowalcze@ericsson.com>
2017-01-25 15:44:45 +0100

    simulator fixed

--- adapter/src/main/java/com/addrone/CommHandlerSimulator.java
+++ adapter/src/main/java/com/addrone/CommHandlerSimulator.java
@@ -6,7 +6,6 @@ import com.multicopter.java.actions.FlightLoopAction;
 import com.multicopter.java.data.*;
 import com.multicopter.java.events.CommEvent;
 import com.multicopter.java.events.MessageEvent;
-import sun.misc.Signal;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -26,13 +25,16 @@ public class CommHandlerSimulator implements CommInterface.CommInterfaceListener
     private CommInterface commInterface;
     private CommDispatcher dispatcher;
 
-    private List<CommTask> runningTasks;
 
     private State state;
-    private Flight_state flight_state;
+    private FlightState flightState;
     private MagnetometerState magnetometerState;
     private UploadControlSettingsStage uploadStage;
     private UploadRouteState uploadRouteState;
+
+    private CalibrationSettings calibrationSettings = getStartCalibrationSettings();
+    private ControlSettings controlSettings = getStartControlSettings();
+    private RouteContainer routeContainer = getStartRouteContainer();
 
     private DebugData debugDataToSend = getStartDebugData();
     private Lock debugDataLock = new ReentrantLock();
@@ -70,7 +72,7 @@ public class CommHandlerSimulator implements CommInterface.CommInterfaceListener
         RUNNING
     }
 
-    private enum Flight_state{
+    private enum FlightState{
         WAITING_FOR_RUNNING,
         RUNNING
     }
@@ -78,10 +80,9 @@ public class CommHandlerSimulator implements CommInterface.CommInterfaceListener
     public CommHandlerSimulator(CommInterface commInterface) {
         this.commInterface = commInterface;
         this.dispatcher = new CommDispatcher(this);
-        this.runningTasks = new ArrayList<>();
 
         this.state = State.IDLE;
-        this.flight_state = Flight_state.WAITING_FOR_RUNNING;
+        this.flightState = FlightState.WAITING_FOR_RUNNING;
     }
 
     @Override
@@ -167,7 +168,7 @@ public class CommHandlerSimulator implements CommInterface.CommInterfaceListener
                     // simulate calibration process (sleep 0.5s)
                     Thread.sleep(500);
                     send(new SignalData(SignalData.Command.CALIBRATION_SETTINGS, SignalData.Parameter.READY).getMessage());
-                    sendCalibrationSettings(new CalibrationSettings());
+                    sendCalibrationSettings(calibrationSettings);
                     connectionStage = ConnectionStage.CALIBRATION_ACK;
                     calibrationSettingsSendingFails = 0;
                 }
@@ -182,12 +183,12 @@ public class CommHandlerSimulator implements CommInterface.CommInterfaceListener
                         new SignalData(SignalData.Command.CALIBRATION_SETTINGS, SignalData.Parameter.DATA_INVALID))) {
                     System.out.println("Sending calibration failed, application reports DATA_INVALID, retransmitting...");
                     calibrationSettingsSendingFails++;
-                    sendCalibrationSettings(new CalibrationSettings());
+                    sendCalibrationSettings(calibrationSettings);
                 } else if (event.matchSignalData(
                         new SignalData(SignalData.Command.CALIBRATION_SETTINGS, SignalData.Parameter.TIMEOUT))) {
                     System.out.println("Sending calibration failed, application reports TIMEOUT, retransmitting...");
                     calibrationSettingsSendingFails++;
-                    sendCalibrationSettings(new CalibrationSettings());
+                    sendCalibrationSettings(calibrationSettings);
                 }
                 if (calibrationSettingsSendingFails >= 3) {
                     throw new Exception("Calibration settings procedure failed, max retransmission limit exceeded!");
@@ -201,7 +202,7 @@ public class CommHandlerSimulator implements CommInterface.CommInterfaceListener
                     state = State.APP_LOOP;
                     // starting debug task
                     debugTask.start();
-                    runningTasks.add(debugTask);
+
                     System.out.println("App loop started");
                 }
                 break;
@@ -221,29 +222,29 @@ public class CommHandlerSimulator implements CommInterface.CommInterfaceListener
                 } else if (event.matchSignalData(new SignalData(SignalData.Command.APP_LOOP, SignalData.Parameter.BREAK))) {
                     System.out.println("Disconnect message received, leaving app loop and disconnecting");
                     // stop all running tasks
-                    runningTasks.forEach(CommTask::stop);
+                    debugTask.stop();
                     send(new SignalData(SignalData.Command.APP_LOOP, SignalData.Parameter.BREAK_ACK).getMessage());
                     commInterface.disconnect();
 
                 } else if (event.matchSignalData(new SignalData(SignalData.Command.CALIBRATE_MAGNET, SignalData.Parameter.START))){
                     System.out.println("Starting magnetometer calibration procedure");
-                    runningTasks.forEach(CommTask::stop);
+                    debugTask.stop();
                     state = State.CALIBRATE_MAGNET;
                     magnetometerState = MagnetometerState.MAGNET_CALIBRATION_IDLE;
                     send(new SignalData(SignalData.Command.CALIBRATE_MAGNET, SignalData.Parameter.ACK).getMessage());
 
                 } else if (event.matchSignalData(new SignalData(SignalData.Command.CALIBRATE_ACCEL, SignalData.Parameter.START))){
                     System.out.println("Starting accelerometer calibration procedure");
-                    runningTasks.forEach(CommTask::stop);
+                    debugTask.stop();
                     state = State.CALIBRATE_ACCEL;
                     send(new SignalData(SignalData.Command.CALIBRATE_ACCEL, SignalData.Parameter.ACK).getMessage());
                     Thread.sleep(500);
                     send(new SignalData(SignalData.Command.CALIBRATE_ACCEL, SignalData.Parameter.DONE).getMessage());
-                    sendCalibrationSettings(new CalibrationSettings());
+                    sendCalibrationSettings(calibrationSettings);
 
                 } else if(event.matchSignalData(new SignalData(SignalData.Command.FLIGHT_LOOP, SignalData.Parameter.START))) {
                     System.out.println("Flight loop started");
-                    runningTasks.forEach(CommTask::stop);
+                    debugTask.stop();
                     send(new SignalData(SignalData.Command.FLIGHT_LOOP, SignalData.Parameter.ACK).getMessage());
                     state = State.FLIGHT_LOOP;
 
@@ -251,26 +252,26 @@ public class CommHandlerSimulator implements CommInterface.CommInterfaceListener
                     System.out.println("Uploading control settings");
                     state = State.UPLOAD_CONTROL_SETTINGS;
                     uploadStage = UploadControlSettingsStage.INIT;
-                    runningTasks.forEach(CommTask::stop);
+                    debugTask.stop();;
                     send(new SignalData(SignalData.Command.UPLOAD_SETTINGS, SignalData.Parameter.ACK).getMessage());
 
                 } else if(event.matchSignalData(new SignalData(SignalData.Command.DOWNLOAD_SETTINGS, SignalData.Parameter.START))){
                     state = State.DOWNLOAD_CONTROL_SETTINGS;
                     System.out.println("Downloading control settings");
-                    runningTasks.forEach(CommTask::stop);
+                    debugTask.stop();
                     send(new SignalData(SignalData.Command.DOWNLOAD_SETTINGS, SignalData.Parameter.ACK).getMessage());
 
                 } else if(event.matchSignalData(new SignalData(SignalData.Command.UPLOAD_ROUTE, SignalData.Parameter.START))){
                     state = State.UPLOAD_ROUTE_CONTAINER;
                     uploadRouteState = UploadRouteState.IDLE;
                     System.out.println("Starting Route Container upload procedure");
-                    runningTasks.forEach(CommTask::stop);
+                    debugTask.stop();
                     send(new SignalData(SignalData.Command.UPLOAD_ROUTE, SignalData.Parameter.ACK).getMessage());
 
                 } else if(event.matchSignalData(new SignalData(SignalData.Command.DOWNLOAD_ROUTE, SignalData.Parameter.ACK))){
                     state = State.DOWNLOAD_ROUTE_CONTAINER;
                     System.out.println("Starting RoutevContainer download procedure");
-                    runningTasks.forEach(CommTask::stop);
+                    debugTask.stop();
                     send(new SignalData(SignalData.Command.DOWNLOAD_ROUTE, SignalData.Parameter.ACK).getMessage());
                 }
             }
@@ -282,12 +283,11 @@ public class CommHandlerSimulator implements CommInterface.CommInterfaceListener
     }
 
     private void handleEventFlightLoop(CommEvent event) throws Exception {
-        switch (flight_state) {
+        switch (flightState) {
             case WAITING_FOR_RUNNING:
                 if (event.matchSignalData(new SignalData(SignalData.Command.FLIGHT_LOOP, SignalData.Parameter.READY))) {
                     debugTask.start();
-                    runningTasks.add(debugTask);
-                    flight_state = Flight_state.RUNNING;
+                    flightState = FlightState.RUNNING;
                     System.out.println("Flight loop ready");
                 }
                 break;
@@ -308,6 +308,7 @@ public class CommHandlerSimulator implements CommInterface.CommInterfaceListener
                                 send(new SignalData(SignalData.Command.FLIGHT_LOOP, SignalData.Parameter.BREAK_ACK).getMessage());
                                 System.out.println("I want make you happy");
                                 state = State.APP_LOOP;
+                                flightState = FlightState.WAITING_FOR_RUNNING;
                             }
                     }
                 }
@@ -323,17 +324,15 @@ public class CommHandlerSimulator implements CommInterface.CommInterfaceListener
                     state = State.APP_LOOP;
                     send(new SignalData(SignalData.Command.CALIBRATE_MAGNET, SignalData.Parameter.ACK).getMessage());
                     debugTask.start();
-                    runningTasks.add(debugTask);
                 } else if(event.matchSignalData(new SignalData(SignalData.Command.CALIBRATE_MAGNET, SignalData.Parameter.DONE))){
                     send(new SignalData(SignalData.Command.CALIBRATE_MAGNET, SignalData.Parameter.DONE).getMessage());
-                    sendCalibrationSettings(new CalibrationSettings());
+                    sendCalibrationSettings(calibrationSettings);
                     magnetometerState = MagnetometerState.MAGNET_CALIBRATION_RUNNING;
                 }  else{
                     System.out.println("Calibration failed");
                     state = State.APP_LOOP;
                     send(new SignalData(SignalData.Command.CALIBRATE_MAGNET, SignalData.Parameter.FAIL).getMessage());
                     debugTask.start();
-                    runningTasks.add(debugTask);
                 }
                 break;
             case MAGNET_CALIBRATION_RUNNING:
@@ -341,15 +340,14 @@ public class CommHandlerSimulator implements CommInterface.CommInterfaceListener
                     System.out.println("Calibration procedure done successfully.");
                     state = State.APP_LOOP;
                     debugTask.start();
-                    runningTasks.add(debugTask);
                 } else if (event.matchSignalData(new SignalData(SignalData.Command.CALIBRATION_SETTINGS, SignalData.Parameter.DATA_INVALID))) {
                     System.out.println("Sending calibration failed, application reports DATA_INVALID, retransmitting...");
                     calibrationSettingsSendingFails++;
-                    sendCalibrationSettings(new CalibrationSettings());
+                    sendCalibrationSettings(calibrationSettings);
                 } else if (event.matchSignalData(new SignalData(SignalData.Command.CALIBRATION_SETTINGS, SignalData.Parameter.TIMEOUT))) {
                     System.out.println("Sending calibration failed, application reports TIMEOUT, retransmitting...");
                     calibrationSettingsSendingFails++;
-                    sendCalibrationSettings(new CalibrationSettings());
+                    sendCalibrationSettings(calibrationSettings);
                 }
                 if (calibrationSettingsSendingFails >= 3) {
                     throw new Exception("Calibration settings procedure failed, max retransmission limit exceeded!");
@@ -363,18 +361,17 @@ public class CommHandlerSimulator implements CommInterface.CommInterfaceListener
                 new SignalData(SignalData.Command.CALIBRATION_SETTINGS, SignalData.Parameter.ACK))) {
             System.out.println("Calibration procedure done successfully.");
             debugTask.start();
-            runningTasks.add(debugTask);
             state = State.APP_LOOP;
         } else if (event.matchSignalData(
                 new SignalData(SignalData.Command.CALIBRATION_SETTINGS, SignalData.Parameter.DATA_INVALID))) {
             System.out.println("Sending calibration failed, application reports DATA_INVALID, retransmitting...");
             calibrationSettingsSendingFails++;
-            sendCalibrationSettings(new CalibrationSettings());
+            sendCalibrationSettings(calibrationSettings);
         } else if (event.matchSignalData(
                 new SignalData(SignalData.Command.CALIBRATION_SETTINGS, SignalData.Parameter.TIMEOUT))) {
             System.out.println("Sending calibration failed, application reports TIMEOUT, retransmitting...");
             calibrationSettingsSendingFails++;
-            sendCalibrationSettings(new CalibrationSettings());
+            sendCalibrationSettings(calibrationSettings);
         }
         if (calibrationSettingsSendingFails >= 3) {
             throw new Exception("Calibration settings procedure failed, max retransmission limit exceeded!");
@@ -397,7 +394,7 @@ public class CommHandlerSimulator implements CommInterface.CommInterfaceListener
                 uploadFails = 0;
                 send(new SignalData(SignalData.Command.CONTROL_SETTINGS, SignalData.Parameter.READY).getMessage());
                 controlSettings.getMessages().forEach(this::send);
-
+                uploadStage = UploadControlSettingsStage.CONTROL_ACK;
                 break;
 
             case CONTROL_ACK:
@@ -498,26 +495,96 @@ public class CommHandlerSimulator implements CommInterface.CommInterfaceListener
         calibrationSettings.getMessages().forEach(this::send);
     }
 
+    private float getRandN() {
+        return (float)(Math.random() - 0.5) * 2.0f;
+    }
+
+    private CalibrationSettings getStartCalibrationSettings() {
+        CalibrationSettings result = new CalibrationSettings();
+        result.setGyroOffset(new float[]{getRandN() * 400.0f, getRandN() * 400.f, getRandN() * 200.0f});
+        result.setFlagState(CalibrationSettings.FlagId.IS_GPS_CONNECTED, true);
+        result.setCrc();
+        return result;
+    }
+
+    private ControlSettings getStartControlSettings() {
+        ControlSettings result = new ControlSettings();
+
+        result.setCrc();
+        return result;
+    }
+
+    private RouteContainer getStartRouteContainer() {
+        RouteContainer result = new RouteContainer();
+
+        result.setCrc();
+        return result;
+    }
+
     private DebugData getStartDebugData() {
         DebugData result = new DebugData();
-        result.setRoll((float)Math.toRadians(23.0));
-        result.setPitch((float)Math.toRadians(13.0));
-        result.setYaw((float)Math.toRadians(53.0));
-        result.setLatitude(50.034f);
-        result.setLongitude(19.940f);
-        result.setRelativeAltitude(24.2f);
-        result.setVLoc(3.2f);
+        result.setRoll((float)Math.toRadians(getRandN() * 30.0f));
+        result.setPitch((float)Math.toRadians(getRandN()));
+        result.setYaw((float)Math.toRadians(getRandN() * 50.0f));
+        result.setLatitude(50.034f + getRandN() / 1000.0f);
+        result.setLongitude(19.940f + getRandN() / 1000.0f);
+        result.setRelativeAltitude(getRandN() * 30.0f);
+        result.setVLoc(getRandN() * 2.0f);
         result.setControllerState(DebugData.ControllerState.APPLICATION_LOOP);
+        result.setFLagState(DebugData.FlagId.GPS_FIX, true);
         result.setFLagState(DebugData.FlagId.GPS_FIX_3D, true);
         return result;
     }
 
+    private float time = (float)(Math.random() * 2);
+
     private void simulateSensors() {
         debugDataLock.lock();
-        // TODO simulate changing of debug data parameters
-        debugDataToSend.setLatitude(debugDataToSend.getLatitude() + (float)Math.random()/10000.0f - 0.00003f);
-        debugDataToSend.setLongitude(debugDataToSend.getLongitude() + (float)Math.random()/10000.0f + 0.00003f);
-        System.out.println(debugDataToSend.getLatitude() + " " + debugDataToSend.getLongitude());
+        time += 1.0f / 25;
+
+        debugDataToSend.setRoll(debugDataToSend.getRoll()
+                + (float)Math.sin(0.53 * time) / 240.0f
+                + (float)Math.sin(1 * time) / 190.0f
+                + (float)Math.sin(5 * time) / 210.0f
+                + (float)Math.sin(2 * time + 1) / 310.0f);
+        debugDataToSend.setPitch(debugDataToSend.getPitch()
+                + (float)Math.sin(0.48 * time) / 330.0f
+                + (float)Math.sin(1.2 * time) / 220.0f
+                + (float)Math.sin(4 * time) / 320.0f
+                + (float)Math.sin(3 * time + 1) / 410.0f);
+        debugDataToSend.setYaw(debugDataToSend.getYaw()
+                + (float)Math.sin(0.24 * time) / 190.0f
+                + (float)Math.sin(0.6 * time + getRandN()) / 50.0f
+                + (float)Math.sin(5 * time + getRandN()) / 90.0f
+                + (float)Math.sin(3 * time + 1) / 150.0f);
+
+        debugDataToSend.setLatitude(debugDataToSend.getLatitude()
+                + (float)Math.sin(0.1 * time) /     250000.0f
+                + (float)Math.sin(0.23 * time) /    280000.0f
+                + (float)Math.sin(0.38 * time) /    300000.0f
+                + (float)Math.sin(2 * time) /       340000.0f
+                + (float)Math.sin(3 * time) /       370000.0f
+                + (float)Math.sin(1 * time) /       250000.0f
+                + getRandN() / 1000000.0f);
+        debugDataToSend.setLongitude(debugDataToSend.getLongitude()
+                + (float)Math.sin(0.22 * time) /    260000.0f
+                + (float)Math.sin(0.56 * time) /    300000.0f
+                + (float)Math.sin(0.8 * time) /     330000.0f
+                + (float)Math.sin(2.1 * time) /     350000.0f
+                + (float)Math.sin(3 * time) /       370000.0f
+                + (float)Math.sin(1.5 * time) /     260000.0f
+                + getRandN() / 1000000.0f);
+
+        debugDataToSend.setVLoc(debugDataToSend.getVLoc()
+                + (float)Math.sin(0.44 * time) / 120.0f
+                + (float)Math.sin(0.9 * time + getRandN()) / 20.0f
+                + (float)Math.sin(5 * time + getRandN()) / 60.0f
+                + (float)Math.sin(1 * time + 1) / 110.0f);
+        debugDataToSend.setRelativeAltitude(debugDataToSend.getRelativeAltitude()
+                + (float)Math.sin(0.74 * time) / 50.0f
+                + (float)Math.sin(0.8 * time + getRandN()) / 10.0f
+                + (float)Math.sin(5 * time + getRandN()) / 70.0f
+                + (float)Math.sin(2 * time + 1) / 110.0f);
         debugDataLock.unlock();
     }
 
