fb0a8f0d151ebeee9d0787d7d836deb0a164ea2d
Bartosz Nawrot <nawbar23@gmail.com>
2017-01-26 16:41:51 +0100

    Optimization fixes for stream forwarding and multicopter uplift

--- server/src/main/java/com/addrone/DataStream.java
+++ server/src/main/java/com/addrone/DataStream.java
@@ -12,7 +12,6 @@ public class DataStream implements Runnable {
     private Server server;
     private DataInputStream input;
     private DataOutputStream output;
-    private byte[] byteArray;
 
     public DataStream(DataInputStream input, DataOutputStream output, Server server) {
         this.input = input;
@@ -22,14 +21,21 @@ public class DataStream implements Runnable {
 
     @Override
     public void run() {
+        byte[] byteArray = new byte[1024];
         while(true) {
             try {
-                byteArray = new byte[input.available()];
-                input.read(byteArray);
-                output.write(byteArray);
+                int len = input.available();
+                if (len > 1024) len = 1024;
+                int read = input.read(byteArray, 0, len);
+                output.write(byteArray, 0, read);
             } catch (IOException e) {
                 e.printStackTrace();
                 server.restoreConnection();
+            }
+            try {
+                Thread.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
         }
     }
